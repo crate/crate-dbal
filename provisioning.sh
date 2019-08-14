c57bc1d439b82c7cfff1b60415ef7f247f8a948a
Sebastian Utz <su@rtme.net>
2019-08-14 11:14:56 +0200

    Switch vagrant test box to use ubuntu 18.04/bionic
    This distribution already includes Java 11 which is required to run
    CrateDB >= 4.0.

--- provisioning.sh
+++ provisioning.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y python-software-properties
-add-apt-repository -y ppa:openjdk-r/ppa
+apt-get upgrade
 add-apt-repository -y ppa:ondrej/php
 wget https://cdn.crate.io/downloads/deb/DEB-GPG-KEY-crate
 apt-key add DEB-GPG-KEY-crate
-touch /etc/apt/sources.list.d/crate.list
-cat <<EOT >> /etc/apt/sources.list.d/crate.list
-deb https://cdn.crate.io/downloads/deb/stable/ trusty main
-deb-src https://cdn.crate.io/downloads/deb/stable/ trusty main
-EOT
+. /etc/os-release
+echo "deb https://cdn.crate.io/downloads/deb/stable/ $UBUNTU_CODENAME main" > /etc/apt/sources.list.d/crate-stable-$UBUNTU_CODENAME.list
 apt-get update
-apt-get install -y crate php7.2-cli php7.2-xml php7.2-curl php7.2-mbstring git zip unzip
+# test classes of DBAL which we depend on are only available inside the source, so DON'T install zip|unzip to force
+# composer to install depencenies from source
+apt-get install -y crate php7.2-cli php7.2-xml php7.2-curl php7.2-mbstring
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 cd /vagrant && su vagrant -c 'composer install'
-// test classes of DBAL which we depend on are only available inside the source
-cd /vagrant && su vagrant -c 'rm -rf vendor/doctrine/dbal && composer update doctrine/dbal --prefer-source'
