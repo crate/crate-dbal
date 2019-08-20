9985e2f062b6f0c1ad0a5793ae9c78c6777037bb
Sebastian Utz <su@rtme.net>
2019-08-20 09:28:58 +0200

    Prepare release 2.0.0

--- src/Crate/DBAL/Driver/PDOCrate/Driver.php
+++ src/Crate/DBAL/Driver/PDOCrate/Driver.php
@@ -30,7 +30,7 @@ use Doctrine\DBAL\VersionAwarePlatformDriver;
 
 class Driver implements \Doctrine\DBAL\Driver, VersionAwarePlatformDriver
 {
-    const VERSION = '1.1.0';
+    const VERSION = '2.0.0';
     const NAME = 'crate';
 
     private const VERSION_057 = '0.57.0';
