d715dc2c5e3409f90112d1280206f29d085f353b
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-03-31 18:24:18 +0200

    Fix Sphinx theme import

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-from crate.theme.rtd.conf import *
+from crate.theme.rtd.conf.clients_dbal import *
 
 from sphinx.highlighting import lexers
 from pygments.lexers.web import PhpLexer
@@ -6,7 +6,6 @@ lexers["php"] = PhpLexer(startinline=True, linenos=1)
 lexers["php-annotations"] = PhpLexer(startinline=True, linenos=1)
 # primary_domain = "php"
 
-project = u'Crate DBAL'
 source_suffix = '.rst'
 html_theme_options.update({
     'canonical_url_path': 'docs/reference/dbal/',
