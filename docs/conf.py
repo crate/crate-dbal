435eb22a6ce569fd9fe1d3bc1f5f08f8fe90b25b
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-05-07 15:45:58 +0200

    Prune config (no longer needed)

--- docs/conf.py
+++ docs/conf.py
@@ -5,12 +5,3 @@ from pygments.lexers.web import PhpLexer
 lexers["php"] = PhpLexer(startinline=True, linenos=1)
 lexers["php-annotations"] = PhpLexer(startinline=True, linenos=1)
 # primary_domain = "php"
-
-source_suffix = '.rst'
-html_theme_options.update({
-    'canonical_url_path': 'docs/dbal/',
-    'tracking_project': 'crate-dbal',
-})
-
-site_url = 'https://crate.io/docs/dbal/en/latest/'
-extensions = ['sphinx_sitemap']
